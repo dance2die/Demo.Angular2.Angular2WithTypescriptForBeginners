516e3ddae088c3603a178561fce5102ac683ad3e
dance2die <dontbothermewithspam@gmail.com>
2016-06-19 08:44:30 -0400

    implemented a menu bar using ngSwitch

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,6 +10,16 @@ import {TweetComponent} from './tweet.component';
 @Component({
     selector: 'my-app',
     template: `
+        <ul class="nav nav-pills">
+            <li [class.active]="viewMode == 'map'"><a (click)="viewMode = 'map'">Map View</a></li>
+            <li [class.active]="viewMode == 'list'"><a (click)="viewMode = 'list'">List View</a></li>
+        </ul>
+        <div [ngSwitch]="viewMode">
+            <template [ngSwitchWhen]="'map'" ngSwitchDefault>Map View Content</template>
+            <template [ngSwitchWhen]="'list'">List View Content</template>
+        </div>
+
+
         <div class="tweets">
             <div *ngFor="#tweet of tweets">
                 <tweet [data]="tweet"></tweet>
@@ -66,6 +76,9 @@ export class AppComponent {
     };
 
     tweets: any;
+    viewMode = 'map';
+
+
 
     constructor(private _tweet: TweetService){
         this.tweets = _tweet.getTweets();
