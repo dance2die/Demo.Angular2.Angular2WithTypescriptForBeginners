f496ec0f5ef99b4927c0448aafa929cf90d95c72
dance2die <dontbothermewithspam@gmail.com>
2016-06-24 05:53:50 -0400

    load image

---
+++ app/signup-form.component.ts
@@ -0,0 +1,31 @@
+import {Component} from 'angular2/core';
+import {ControlGroup, Control, Validators, FormBuilder} from "angular2/common";
+import {UsernameValidators} from './usernameValidators';
+
+@Component({
+    selector: 'signup-form',
+    templateUrl: 'app/signup-form.component.html'
+})
+export class SignUpFormComponent {
+    form: ControlGroup;
+
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            username: ['', Validators.compose([
+                Validators.required,
+                UsernameValidators.cannotContainSpace]),
+                UsernameValidators.shouldBeUnique],
+            password: ['', Validators.required]
+        }); 
+    }
+
+    signup(){
+        // var result = authSerivce.login(this.form.value);
+        this.form.find('username').setErrors({
+            invalidLogin: true
+        });
+        
+        
+        console.log(this.form.value);
+    }
+}
